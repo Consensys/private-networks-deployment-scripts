9e9ff35986b84eb9abb49f55d2258a85277bb7c7
Antonina Norair <antonina.norair@gmail.com>
2018-01-19 14:55:54 -0800

    Add few changes + initiation of network

--- vagrant-vm/Nnodes/init-network.sh
+++ vagrant-vm/Nnodes/init-network.sh
@@ -5,11 +5,14 @@ echo 'Init private PoA network'
 echo 'Enter name of your private network'
 read network_name
 
+NETWORK_DIR=${network_name}_network
+NETWORK_GENESIS_FILE=${network_name}_genesis.json
+
 #Clean up previous network
 #TODO instead of deleting move to back folder
-rm -rf ${network_name}_chain
-mkdir ${network_name}_chain
-cd ${network_name}_chain
+rm -rf $NETWORK_DIR
+mkdir $NETWORK_DIR
+cd $NETWORK_DIR
 mkdir logs
 
 echo 'Enter number of nodes'
@@ -38,13 +41,19 @@ done
 PUPPETH_ARG+="\n"
 
 #GENERATE GENESIS FILE
-PUPPETH_ARG+="2\n2\n${network_name}_genesis.json\n"
+PUPPETH_ARG+="2\n2\n$NETWORK_GENESIS_FILE\n"
 
 echo "PUPPETH_ARG = $PUPPETH_ARG"
 
 
 #CALL puppeth network manager and pass arguments
 printf $PUPPETH_ARG | puppeth &> logs/puppeth_output.log
+
+#INIT network nodes
+for ((i = 1; i <=$num; i++)) {
+ echo "Init node $i" 
+ geth --datadir nodes/node$i init $NETWORK_GENESIS_FILE
+}
 
 cd ../
 
